eeaaec7d46eb174748cc212d6309328aac5e209b
Casey Brannon <cbrannon412@gmail.com>
2019-12-11 15:37:42 -0500

    added form to create

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import {View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput,Button, StyleSheet } from 'react-native';
 
 const CreateScreen = () => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
     return (
         <View>
-            <Text>Create Screen</Text>
+            <Text style={styles.lable}>Enter Title</Text>
+            <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)}/>
+            <Text style={styles.lable}>Enter Content</Text>
+            <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)}/>
+            <Button title="Add Post" />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 15,
+        padding: 5,
+        margin: 5
+    },
+    lable: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5
+
+    }
+});
 
 export default CreateScreen;
