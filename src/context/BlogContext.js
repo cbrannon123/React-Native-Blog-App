ce61afc418a47786e6df38b4321a9a061409d6b9
Casey Brannon <cbrannon412@gmail.com>
2019-12-12 16:31:23 -0500

    edit complete

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,6 +3,12 @@ import createDataContext from './createDataContext'
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                return blogPost.id === action.payload.id
+                    ? action.payload
+                    : blogPost
+            })
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload);
         case 'add_blogpost':
@@ -20,7 +26,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
     return (title, content, callback) => {
         dispatch({ type: 'add_blogpost', payload: { title, content } });
-        callback();
+        if (callback) {
+            callback();
+        }
     };
 };
 
@@ -30,7 +38,21 @@ const deleteBlogPost = (dispatch) => {
     };
 };
 
+const editBlogPost = (dispatch) => {
+    return (id, title, content, callback) => {
+        dispatch({
+            type: 'edit_blogpost',
+            payload: {
+                id, title, content
+            }
+        });
+        if (callback) {
+            callback()
+        }
+    };
+}
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [{ title: 'Testpost', content: 'Test Content', id: 1}])
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost }, [{ title: 'Testpost', content: 'Test Content', id: 1 }])
 
 
