2a5ba39025f29267f4dc59b4d423c9b0909e0a98
Casey Brannon <cbrannon412@gmail.com>
2019-12-12 16:08:50 -0500

    create and edit

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -31,6 +31,6 @@ const deleteBlogPost = (dispatch) => {
 };
 
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [])
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [{ title: 'Testpost', content: 'Test Content', id: 1}])
 
 
